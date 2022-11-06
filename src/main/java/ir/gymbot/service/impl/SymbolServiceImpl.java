c5ad384776bc0081782bfe20c089b8146d196db6
Mohammad Jalili <mo.jalili73@gmail.com>
2022-11-06 17:24:21 +0330

    add db config

--- src/main/java/ir/gymbot/service/impl/SymbolServiceImpl.java
+++ src/main/java/ir/gymbot/service/impl/SymbolServiceImpl.java
@@ -2,17 +2,23 @@ package ir.gymbot.service.impl;
 
 import static org.elasticsearch.index.query.QueryBuilders.*;
 
+import com.kucoin.sdk.KucoinClientBuilder;
+import com.kucoin.sdk.KucoinRestClient;
+import com.kucoin.sdk.rest.response.SymbolResponse;
 import ir.gymbot.domain.Symbol;
 import ir.gymbot.repository.SymbolRepository;
 import ir.gymbot.repository.search.SymbolSearchRepository;
 import ir.gymbot.service.SymbolService;
 import ir.gymbot.service.dto.SymbolDTO;
 import ir.gymbot.service.mapper.SymbolMapper;
+import java.io.IOException;
+import java.util.List;
 import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -103,5 +109,13 @@ public class SymbolServiceImpl implements SymbolService {
     public Page<SymbolDTO> search(String query, Pageable pageable) {
         log.debug("Request to search for a page of Symbols for query {}", query);
         return symbolSearchRepository.search(query, pageable).map(symbolMapper::toDto);
+    }
+
+    @Scheduled(initialDelay = 1, fixedRate = 1000 * 60 * 60 * 24 * 7)
+    public void getSymbols() throws IOException {
+        KucoinClientBuilder builder = new KucoinClientBuilder().withBaseUrl("https://openapi-v2.kucoin.com");
+        KucoinRestClient kucoinRestClient = builder.buildRestClient();
+        List<SymbolResponse> symbols = kucoinRestClient.symbolAPI().getSymbols();
+        System.out.println(symbols);
     }
 }
